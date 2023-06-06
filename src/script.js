118191e05f608834d3587e13009771da50df649d
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-06-06 22:00:13 +0530

    feat : Add header & navbar

--- src/script.js
+++ src/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
 /**
+ * add evnet listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, evnetType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(evnetType, callback);
+  }
+};
+
+/**
  * PRELOADER
  */
 
@@ -10,3 +20,22 @@ window.addEventListener("DOMContentLoaded", function () {
   preloader.classList.add("loaded");
   document.body.classList.add("loaded");
 });
+
+/**
+ * NAVBAR
+ * navbar toggle for mobile
+ */
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navTogglerBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navTogglerBtn.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
