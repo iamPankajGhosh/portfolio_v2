de66eee74ec60c64a383879a7988c4c3fe6be91d
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-06-07 13:17:03 +0530

    feat : Add service section

--- src/script.js
+++ src/script.js
@@ -54,3 +54,98 @@ window.addEventListener("scroll", function () {
     header.classList.remove("active");
   }
 });
+
+/**
+ * SLIDER
+ */
+
+const sliders = document.querySelectorAll("[data-slider]");
+const initSlider = function (currentSlider) {
+  const sliderContainer = currentSlider.querySelector(
+    "[data-slider-container]"
+  );
+  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+  let totalSliderVisibleItems = Number(
+    getComputedStyle(currentSlider).getPropertyValue("--slider-items")
+  );
+  let totalSlidebleItems =
+    sliderContainer.childElementCount - totalSliderVisibleItems;
+
+  let currentSlidePos = 0;
+
+  const moveSliderItem = function () {
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+  };
+
+  /**
+   * NEXT SLIDE
+   */
+
+  const slideNext = function () {
+    const slideEnd = currentSlidePos >= totalSlidebleItems;
+
+    if (slideEnd) {
+      currentSlidePos = 0;
+    } else {
+      currentSlidePos++;
+    }
+
+    moveSliderItem();
+  };
+
+  sliderNextBtn.addEventListener("click", slideNext);
+
+  /**
+   * PREV SLIDE
+   */
+
+  const slidePrev = function () {
+    const slideEnd = currentSlidePos >= totalSlidebleItems;
+
+    if (currentSlidePos <= 0) {
+      currentSlidePos = totalSlidebleItems;
+    } else {
+      currentSlidePos--;
+    }
+
+    moveSliderItem();
+  };
+
+  sliderPrevBtn.addEventListener("click", slidePrev);
+
+  const dontHaveExtraItem = totalSlidebleItems <= 0;
+  if (dontHaveExtraItem) {
+    sliderNextBtn.style.disply = "none";
+    sliderPrevBtn.style.disply = "none";
+  }
+
+  /**
+   * slide with [shift + mouse wheel]
+   */
+
+  currentSlider.addEventListener("wheel", function (event) {
+    if (event.shiftKey && event.deltaY > 0) slideNext();
+    if (event.shiftKey && event.deltaY < 0) slidePrev();
+  });
+
+  /**
+   * RESPONSIVE
+   */
+
+  window.addEventListener("resize", function () {
+    totalSliderVisibleItems = Number(
+      getComputedStyle(currentSlider).getPropertyValue("--slider-items")
+    );
+
+    totalSlidebleItems =
+      sliderContainer.childElementCount - totalSliderVisibleItems;
+
+    moveSliderItem();
+  });
+};
+
+for (let i = 0, len = sliders.length; i < len; i++) {
+  initSlider(sliders[i]);
+}
